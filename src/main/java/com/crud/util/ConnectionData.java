8ed04b0366c95bc143e1cf1e84f14d4ca73004c5
MSI CYBORG 15 <MSI CYBORG 15@DESKTOP-INH4J1H>
2025-05-06 23:15:39 +0530

    --Commited by PromodM

--- src/main/java/com/crud/util/ConnectionData.java
+++ src/main/java/com/crud/util/ConnectionData.java
@@ -4,5 +4,5 @@ public class ConnectionData {
 
     public static final String DATABASE = "sample_crud";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "61558524";
+    public static final String PASSWORD = "1969Malani@";
 }
